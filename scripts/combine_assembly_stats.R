5868b3a454d02c1f371bae9d39839be83bf6039b
taylorreiter <taylorreiter@gmail.com>
2021-09-19 15:24:10 -0700

    typos

--- scripts/combine_assembly_stats.R
+++ scripts/combine_assembly_stats.R
@@ -5,12 +5,12 @@ library(readr)
 total_bp <- unlist(snakemake@input[["bp"]]) %>%
     set_names() %>%
     map_dfr(read_tsv, col_names = "total_bp", .id = "assembly") %>%
-    mutate(assembly = gsub("_total_bp.txt", "", assembly)
+    mutate(assembly = gsub("_total_bp.txt", "", assembly))
 
 coding_aa <- unlist(snakemake@input[["aa"]]) %>%
     set_names() %>%
     map_dfr(read_tsv, col_names = "cds_aa", .id = "assembly") %>%
-    mutate(assembly = gsub("_aa.txt", "", assembly) %>%
+    mutate(assembly = gsub("_aa.txt", "", assembly)) %>%
     mutate(cds_bp = cds_aa * 3)
 
 assembly_stats <- left_join(total_bp, coding_aa, by = "assembly")
